cbb9f27c80f4ef38bf9b0ffb1012d0f179765289
francesco.malagisi <francesco.malagisi@accenture.com>
2022-06-20 22:27:55 +0200

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,19 @@ import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href='https://github.com/solidjs/solid'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <main class={styles.App}>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        class={styles.link}
+        href='https://github.com/solidjs/solid'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn Solid
+      </a>
+    </main>
   );
 };
 
