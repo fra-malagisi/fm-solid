b73f15a39c9748c69dab49929cd9e3bf3c8b7b9d
francesco.malagisi <francesco.malagisi@accenture.com>
2022-06-26 17:52:01 +0200

    feat: created homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,13 @@
 import { Component } from 'solid-js';
-import { Button } from '../../components/Button';
+import { Button } from '../components/Button';
 import { useNavigate } from 'solid-app-router';
 
 import styles from './Home.module.css';
-import { style } from 'solid-js/web';
 
 const Home: Component = () => {
   const navigate = useNavigate();
 
-  const handleStart = () => {
-    console.log('prova');
-    navigate('/match', { replace: true });
-  };
-
+  const handleStart = () => navigate('/match', { replace: true });
   return (
     <div class={styles.Home}>
       <Button label='Start' handleClick={handleStart} />
