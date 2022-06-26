3ed91af1bd92dabf59ea61f88e157107b3c235e3
francesco.malagisi <francesco.malagisi@accenture.com>
2022-06-26 17:50:14 +0200

    feat: added property isBig on button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { Component, JSX } from 'solid-js';
 
 import styles from './Button.module.css';
@@ -5,11 +6,17 @@ import styles from './Button.module.css';
 export type ButtonProps = {
   label: string;
   handleClick?: () => void;
+  isBig?: boolean;
 } & JSX.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: Component<ButtonProps> = ({ label, type, handleClick }) => {
+const Button: Component<ButtonProps> = ({ label, type, isBig, handleClick }) => {
+  const buttonStyle = clsx({
+    [styles.button]: true,
+    [styles['button--big']]: isBig,
+  });
+
   return (
-    <button type={type} class={styles.button} onClick={() => handleClick && handleClick()}>
+    <button type={type} class={buttonStyle} onClick={() => handleClick && handleClick()}>
       {label}
     </button>
   );
