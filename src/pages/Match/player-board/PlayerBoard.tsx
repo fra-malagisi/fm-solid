b57c46491da145a42c548af47489176ddc2e3397
francesco.malagisi <francesco.malagisi@accenture.com>
2022-06-30 18:19:00 +0200

    feat: added PlayerSection and random-number in PlayerBoard

--- src/pages/Match/player-board/PlayerBoard.tsx
+++ src/pages/Match/player-board/PlayerBoard.tsx
@@ -1,17 +1,20 @@
 import { Component } from 'solid-js';
+import { PlayerSection } from '../player-section';
 
 import styles from './PlayerBoard.module.css';
 
 export type PlayerBoardProps = {
   numberOfPlayers: number;
+  chances: number;
 };
 
-const PlayerBoard: Component<PlayerBoardProps> = ({ numberOfPlayers }) => {
+const PlayerBoard: Component<PlayerBoardProps> = ({ numberOfPlayers, chances }) => {
   return (
     <section class={styles['player-board']}>
       {Array.from(Array(numberOfPlayers).keys()).map(player => (
-        <div class={styles['player-board__player-section']}>{player + 1}</div>
+        <PlayerSection player={player + 1} chances={chances} />
       ))}
+      <div class={styles['player-board__random-number']}></div>
     </section>
   );
 };
