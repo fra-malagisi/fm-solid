03e79d81cf8f7743e188f9f611d66e599b01e51b
francesco.malagisi <francesco.malagisi@accenture.com>
2022-06-30 18:18:16 +0200

    feat: created ChanceMarker component

---
+++ src/pages/Match/chance-marker/ChanceMarker.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'solid-js';
+import styles from './ChanceMarker.module.css';
+
+const ChanceMarker: Component = () => {
+  return <div class={styles['chance-marker']}></div>;
+};
+
+export default ChanceMarker;
---
+++ src/pages/Match/chance-marker/index.ts
@@ -0,0 +1,1 @@
+export { default as ChanceMarker } from './ChanceMarker';
